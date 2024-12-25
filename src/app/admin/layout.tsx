2c67e0f17c4adb734c6d38fc97da364375f4c5bd
nick_bron <nick_bron@ukr.net>
2024-12-25 18:06:45 +0200

    create Supabase Authentication

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,31 @@
+import { ADMIN } from "@/constants/constants";
+import { createClient } from "@/supabase/server";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+export default async function AdminLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  const supabase = await createClient();
+
+  const { data: authData } = await supabase.auth.getUser();
+
+  if (authData?.user) {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", authData.user.id)
+      .single();
+
+    if (error || !data) {
+      console.log("Error fetching user data", error);
+      return;
+    }
+
+    if (data.type === ADMIN) return redirect("/");
+  }
+
+  return <main className="min-h-[calc(100svh-128px)] py-3">{children}</main>;
+}
